1444b17be3fa4939d4b40acbcae0baf91af77c3a
NeymarL <liuhe@bingyan.net>
2016-05-09 22:43:36 +0800

    finish 4 day

--- liuOS/bootpack.c
+++ liuOS/bootpack.c
@@ -2,24 +2,114 @@
 /*     main.c    */
 /*****************/
 
-//#include "header.h"
+#define COL8_000000     0
+#define COL8_FF0000     1
+#define COL8_00FF00     2
+#define COL8_FFFF00     3
+#define COL8_0000FF     4
+#define COL8_FF00FF     5
+#define COL8_00FFFF     6
+#define COL8_FFFFFF     7
+#define COL8_C6C6C6     8
+#define COL8_840000     9
+#define COL8_008400     10
+#define COL8_848400     11
+#define COL8_000084     12
+#define COL8_840084     13
+#define COL8_008484     14
+#define COL8_848484     15
 
-void test();
-void io_hlt();
-void write_mem8(int addr, int data);
+#define VRAM            0xa0000         // vedio ram
+#define XSIZE           320
+#define YSIZE           200
+
+// assembly functions
+void io_hlt();                          // halt
+void io_cli();                          // clear interrupt flag
+void io_out8(int port, int data);
+int io_load_eflags();
+void io_store_eflags(int eflags);
+void write_mem8(int addr, int data);    // write data to address addr
+
+// C functions 
+void init_palette();
+void set_palette(int start, int end, unsigned char* rgb);
+void draw_rect(int color, int x0, int y0, int x1, int y1);
 
 int main()
 {
-    int i;   
-    //char *p;
-    for (i = 0xa0000; i <= 0xaffff; i++){
-        write_mem8(i, 14);
-        //p = (char*)i;
-        //*p = 14;
-    }
-    //test();
+    init_palette();     // 设定调色板
+
+    draw_rect(COL8_008484,  0, 0,          XSIZE -  1, YSIZE - 29);
+    draw_rect(COL8_C6C6C6,  0, YSIZE - 28, XSIZE -  1, YSIZE - 28);
+    draw_rect(COL8_FFFFFF,  0, YSIZE - 27, XSIZE -  1, YSIZE - 27);
+    draw_rect(COL8_C6C6C6,  0, YSIZE - 26, XSIZE -  1, YSIZE -  1);
+
+    draw_rect(COL8_FFFFFF,  3, YSIZE - 24, 59, YSIZE - 24);
+    draw_rect(COL8_FFFFFF,  2, YSIZE - 24,  2, YSIZE -  4);
+    draw_rect(COL8_848484,  3, YSIZE -  4, 59, YSIZE -  4);
+    draw_rect(COL8_848484, 59, YSIZE - 23, 59, YSIZE -  5);
+    draw_rect(COL8_000000,  2, YSIZE -  3, 59, YSIZE -  3);
+    draw_rect(COL8_000000, 60, YSIZE - 24, 60, YSIZE -  3);
+
+    draw_rect(COL8_848484, XSIZE - 47, YSIZE - 24, XSIZE -  4, YSIZE - 24);
+    draw_rect(COL8_848484, XSIZE - 47, YSIZE - 23, XSIZE - 47, YSIZE -  4);
+    draw_rect(COL8_FFFFFF, XSIZE - 47, YSIZE -  3, XSIZE -  4, YSIZE -  3);
+    draw_rect(COL8_FFFFFF, XSIZE -  3, YSIZE - 24, XSIZE -  3, YSIZE -  3);
+
     while(1){
         io_hlt();
     }
     return 0;
+}
+
+
+void draw_rect(int color, int x0, int y0, int x1, int y1)
+{
+    int vram = VRAM;
+    int xsize = 320;
+    for (int y = y0; y <= y1; y++){
+        for (int x = x0; x <= x1; x++){
+            //vram[y * xsize + x] = color;
+            write_mem8(vram + x + y * xsize, color);
+        }
+    }
+}
+
+void init_palette()
+{
+    static unsigned char table_rgb[16 * 3] = {  
+        0x00, 0x00, 0x00,   // 0. black
+        0xff, 0x00, 0x00,   // 1. light red
+        0x00, 0xff, 0x00,   // 2. light green
+        0xff, 0xff, 0x00,   // 3. light yellow
+        0x00, 0x00, 0xff,   // 4. light blue
+        0xff, 0x00, 0xff,   // 5. light purple
+        0x00, 0xff, 0xff,   // 6. shadow light blue
+        0xff, 0xff, 0xff,   // 7. white
+        0xc6, 0xc6, 0xc6,   // 8. light grey
+        0x84, 0x00, 0x00,   // 9. dark red
+        0x00, 0x84, 0x00,   // 10. dark green
+        0x84, 0x84, 0x00,   // 11. dark yellow
+        0x00, 0x00, 0x84,   // 12. dark qing
+        0x84, 0x00, 0x84,   // 13. dark purple
+        0x00, 0x84, 0x84,   // 14. shadow dark blue
+        0x84, 0x84, 0x84    // 15. dark grey
+    };                      // static char 相当于汇编中的 DB
+    set_palette(0, 15, table_rgb);
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+    int i, eflags;
+    eflags = io_load_eflags();  // 记录中断许可标志的值
+    io_cli();                   // 将中断许可标志置0,禁止中断
+    io_out8(0x03c8, start);     // 将想要设定的调色板号码写入 0x03c8
+    for (i = start; i <= end; i++){
+        io_out8(0x03c9, rgb[0] / 4);
+        io_out8(0x03c9, rgb[1] / 4);
+        io_out8(0x03c9, rgb[2] / 4);
+        rgb += 3;
+    }
+    io_store_eflags(eflags);    // 恢复中断许可标志
 }
--- liuOS/func.nas
+++ liuOS/func.nas
@@ -1,37 +1,88 @@
 ; func
 ; TAB=4
 
-;[FORMAT "WCOFF"]        ; 制作32位目标文件格式
 [BITS 32]               ; 制作32位模式用的机器语言
 
-; 制作目标文件的信息
-
-;[FILE "func.nas"]       ; 源文件名信息
-
-        GLOBAL      io_hlt     ; 程序中包涵的函数名
-        GLOBAL      test
+        GLOBAL      io_hlt, io_cli, io_sti, io_stihlt  
         GLOBAL      write_mem8
+        GLOBAL      io_in8, io_in16, io_in32
+        GLOBAL      io_out8, io_out16, io_out32
+        GLOBAL      io_load_eflags, io_store_eflags
 
 ; 以下是实际函数
 
-io_hlt:                ; void io_hlt(voi
-        HLT
-        RET
-
-test:
-  MOV   EAX, 0xa0000
-  JMP   loops
-  RET
-
-loops:
-  MOV   BYTE [EAX],14
-  INC   EAX
-  CMP   EAX, 0xaffff
-  JBE   loops
-  RET
-
-write_mem8:
-    MOV     EAX, [ESP + 12]
-    MOV     AL, [ESP + 41]
-    MOV     [EAX], AL
+io_hlt:     ; void io_hlt();
+    HLT
     RET
+
+write_mem8: ; void write_mem8(int addr, int data);
+    MOV     ECX, EDI
+    MOV     EAX, 0
+    MOV     AX, SI
+    ;MOV     ECX, [EBP + 8]
+    ;MOV     EAX, [EBP + 12]
+    MOV     [ECX], AL
+    RET
+
+io_cli:     ; void io_cli();
+    CLI 
+    RET
+
+io_sti:     ; void io_sti();
+    STI 
+    RET
+
+io_stihlt:  ; void io_stihlt();
+    STI 
+    HLT 
+    RET
+
+io_in8:     ; int io_in8(int port);
+    MOV     EDX, EDI ; port
+    MOV     EAX, 0
+    IN      AL, DX
+    RET
+
+io_in16:    ; int io_in16(int port);
+    MOV     EDX, EDI ; port
+    MOV     EAX, 0
+    IN      AX, DX
+    RET             ; 执行RET语句时，EAX的值就被看作是函数的返回值
+
+io_in32:    ; int io_in32(int port);
+    MOV     EDX, EDI ; port
+    IN      EAX, DX
+    RET
+
+io_out8:   ; void io_out8(int port, int data);
+    MOV     EDX, EDI ; port
+    MOV     EAX, 0
+    MOV     AX, SI          ; data
+    OUT     DX, AX
+    RET
+
+io_out16:   ; void io_out16(int port, int data);
+    MOV     EDX, EDI ; port
+    MOV     EAX, 0
+    MOV     AX, SI          ; data
+    OUT     DX, AX
+    RET
+
+io_out32:   ; void io_out32(int port, int data);
+    MOV     EDX, EDI ; port
+    MOV     EAX, 0
+    MOV     EAX, ESI        ; data
+    OUT     DX, EAX
+    RET
+
+io_load_eflags: ; int io_load_eflags();
+    PUSHFD      ; push flags double word
+    POP     EAX
+    RET
+
+io_store_eflags:    ; void io_store_eflags();
+    MOV     EAX, EDI
+    PUSH    EAX
+    POPFD           ; pop flags double word
+    RET
+
