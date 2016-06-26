afb449b02e23d7b4261e8216cc1bbedf0ce024e7
NeymarL <liuhe@bingyan.net>
2016-06-26 22:59:43 +0800

    update

--- projects/04_day/harib01d/bootpack.c
+++ projects/04_day/harib01d/bootpack.c
@@ -5,10 +5,10 @@ void HariMain(void)
 	int i; /* 変数宣言。iという変数は、32ビットの整数型 */
 	char *p; /* pという変数は、BYTE [...]用の番地 */
 
-	p = (char *) 0xa0000; /* 番地を代入 */
+	char *p = (char *) 0xa0000;
 
-	for (i = 0; i <= 0xffff; i++) {
-		*(p + i) = i & 0x0f;
+	for (int i = 0; i <= 0xffff; i++) {
+		*(p + i) = 0x0f;
 	}
 
 	for (;;) {
